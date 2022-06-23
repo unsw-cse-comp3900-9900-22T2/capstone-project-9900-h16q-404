a37992bd796542a2d9b9fca199fa5a635ba83e19
bengbmccoy <>
2022-06-24 08:48:19 +1000

    cleanup of app.py

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 #CORS(app) #comment this on deployment
 api = Api(app)
 
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
-
 api.add_resource(ApiHandler, '/flask/hello')
 
 api.add_resource(SignUp, '/signup')
