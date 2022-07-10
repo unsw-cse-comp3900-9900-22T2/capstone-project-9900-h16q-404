77be631c35ba919dbd3ddb88067bdba56e8adcc1
陈韵然 <cnlightsoul@zens-MacBook-Pro.local>
2022-07-10 14:44:35 +1000

    splited search bar content to words

--- frontend/src/components/search_input.jsx
+++ frontend/src/components/search_input.jsx
@@ -4,7 +4,16 @@ import { Input, Space } from 'antd';
 const { Search } = Input;
 
 // to be changed when search function is ready to develop
-const onSearch = (value) => console.log(value);
+const onSearch = (value) => {
+  const keyWordList = value.split(" ");
+  const keyWordDict = {
+    keyWordList: keyWordList.map(word => {
+      return word.toLowerCase();
+    })
+  }
+  // when backend ready, change this to axios function
+  console.log(JSON.stringify(keyWordDict));
+}
 
 export default function SearchInput () {
   return(
