f933f5a17a41626529603b1862646d36038c92ac
Dezhao Chen <dezhao.chen@student.unsw.edu.au>
2022-07-19 11:05:28 +1000

    style change

--- frontend/src/pages/buyticket.jsx
+++ frontend/src/pages/buyticket.jsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Layout, Select, Button, Form, Input, Space, Cascader } from 'antd';
+import {
+  Layout,
+  Select,
+  Button,
+  Form,
+  Input,
+  Space,
+  Cascader,
+  message,
+} from 'antd';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import PageHeader from '../components/page_header';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import moment from 'moment';
-const { Option } = Select;
-const { Content } = Layout;
+
+
+const { Content, Footer } = Layout;
 
 const formItemLayout = {
   labelCol: {
@@ -26,6 +35,7 @@ const formItemLayout = {
     },
   },
 };
+
 const formItemLayoutWithOutLabel = {
   wrapperCol: {
     xs: {
@@ -38,11 +48,69 @@ const formItemLayoutWithOutLabel = {
     },
   },
 };
-
 export default function BuyTicket() {
   const params = useParams();
   const eventid = params.eventid;
-  const dict = { gold: [1, 2, 3, 5, 6, 10], silver: [1, 3, 5, 7], bronze: [0] };
+  const navigate = useNavigate();
+  const [token, setToken] = useState();
+  // const dict = { gold: [1, 2, 3, 5, 6, 10], silver: [1, 3, 5, 7], bronze: [0] };
+
+  const [options, setOptions] = useState();
+
+  const filter = (inputValue, path) => {
+    path.some(
+      (option) =>
+        option.label.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
+    );
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('username')) {
+      setToken(localStorage.getItem('username'));
+    } else {
+      message.error('You must log in to do this!');
+      navigate(-1);
+    }
+
+    axios
+      .get(`http://127.0.0.1:5000/buytickets`, {
+        params: {
+          event_id: eventid,
+        },
+      })
+      .then((res) => {
+        if (res.data.resultStatus === 'SUCCESS') {
+          const newdata = {
+            gold: [],
+            silver: [],
+            bronze: [],
+          };
+          for (const value of res.data.message.result) {
+            newdata[value.tix_class].push(value.seat_num);
+          }
+          const newoptions = [];
+          for (const [key, value] of Object.entries(newdata)) {
+            if (value.length > 0) {
+              console.log(value);
+              const option = {
+                value: key,
+                label: key.toString(),
+                children: value.map((item) => {
+                  return {
+                    value: item,
+                    label: item.toString(),
+                  };
+                }),
+              };
+              newoptions.push(option);
+            }
+          }
+          setOptions(newoptions);
+        } else {
+          message.error(res.data.message);
+        }
+      });
+  }, [setOptions]);
 
   const onFinish = (values) => {
     console.log(values);
@@ -60,115 +128,109 @@ export default function BuyTicket() {
         }
       }
     }
-    console.log(tickets);
-  };
-  const [options, setOptions] = useState();
-
-  const filter = (inputValue, path) => {
-    path.some(
-      (option) =>
-        option.label.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
-    );
-  };
-
-  useEffect(() => {
-    const newoptions = [];
-    for (const [key, value] of Object.entries(dict)) {
-      if (!value.includes(0)) {
-        console.log(value);
-        const option = {
-          value: key,
-          label: key.toString(),
-          children: value.map((item) => {
-            return {
-              value: item,
-              label: item.toString(),
-            };
-          }),
-        };
-        newoptions.push(option);
-      }
+    if (tickets.length === 0) {
+      message.warning('At least 1 ticket');
+      return;
     }
-    setOptions(newoptions);
-  }, [setOptions]);
+    console.log(tickets);
+    axios
+      .post(`http://127.0.0.1:5000/buytickets`, {
+        token: token,
+        tickets: tickets,
+      })
+      .then((res) => {
+        if (res.data.resultStatus === 'SUCCESS') {
+          message.success(res.data.message);
+        } else {
+          message.error(res.data.message);
+        }
+      });
+  };
+
   return (
     <div>
-      <h3>Hallo\</h3>
-      <Form name='buyticket' onFinish={onFinish}>
-        <Form.List
-          name='tickets'
-          rules={[
-            {
-              validator: async (_, tickets) => {
-                if (!tickets || tickets.length < 1) {
-                  return Promise.reject(new Error('At least 1 ticket'));
-                }
-              },
-            },
-          ]}
+      <Layout>
+        <PageHeader />
+        <Content
+          className='site-layout'
+          style={{ padding: '0 50px', marginTop: 64 }}
         >
-          {(fields, { add, remove }, { errors }) => (
-            <>
-              {fields.map((field, index) => (
-                <Form.Item
-                  {...(index === 0
-                    ? formItemLayout
-                    : formItemLayoutWithOutLabel)}
-                  label={index === 0 ? 'Ticket' : ''}
-                  required={false}
-                  key={field.key}
-                >
-                  <Form.Item {...field} noStyle>
-                    <Cascader
+          <Form
+            name='buyticket'
+            {...formItemLayoutWithOutLabel}
+            onFinish={onFinish}
+          >
+            <Form.List
+              name='tickets'
+              rules={[
+                {
+                  validator: async (_, tickets) => {
+                    if (!tickets || tickets.length < 1) {
+                      return Promise.reject(new Error('At least 1 ticket'));
+                    }
+                  },
+                },
+              ]}
+            >
+              {(fields, { add, remove }, { errors }) => (
+                <>
+                  {fields.map((field, index) => (
+                    <Form.Item
+                      {...(index === 0
+                        ? formItemLayout
+                        : formItemLayoutWithOutLabel)}
+                      label={index === 0 ? 'Ticket' : ''}
+                      required={false}
+                      key={field.key}
+                    >
+                      <Space>
+                        <Form.Item {...field} noStyle>
+                          <Cascader
+                            style={{
+                              maxWidth: '600px',
+                              minWidth: '200px'
+                            }}
+                            options={options}
+                            showSearch={{
+                              filter,
+                            }}
+                          />
+                        </Form.Item>
+                        {fields.length > 1 ? (
+                          <MinusCircleOutlined
+                            className='dynamic-delete-button'
+                            onClick={() => remove(field.name)}
+                          />
+                        ) : null}
+                      </Space>
+                    </Form.Item>
+                  ))}
+                  <Form.Item>
+                    <Button
+                      type='dashed'
+                      onClick={() => add()}
                       style={{
-                        width: '40%',
+                        maxwidth: '600px',
+                        minWidth: '200px',
                       }}
-                      options={options}
-                      showSearch={{
-                        filter,
-                      }}
-                    />
+                      icon={<PlusOutlined />}
+                    >
+                      Add ticket
+                    </Button>
+                    <Form.ErrorList errors={errors} />
                   </Form.Item>
-                  {fields.length > 1 ? (
-                    <MinusCircleOutlined
-                      className='dynamic-delete-button'
-                      onClick={() => remove(field.name)}
-                    />
-                  ) : null}
-                </Form.Item>
-              ))}
-              <Form.Item>
-                <Button
-                  type='dashed'
-                  onClick={() => add()}
-                  style={{
-                    width: '60%',
-                  }}
-                  icon={<PlusOutlined />}
-                >
-                  Add ticket
-                </Button>
-                <Form.ErrorList errors={errors} />
-              </Form.Item>
-            </>
-          )}
-        </Form.List>
-        <Form.Item>
-          <Button type='primary' htmlType='submit'>
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-
-      <Cascader
-        options={options}
-        showSearch={{
-          filter,
-        }}
-        onChange={(value, selectedOptions) => {
-          console.log(value);
-        }}
-      />
+                </>
+              )}
+            </Form.List>
+            <Form.Item>
+              <Button type='primary' htmlType='submit'>
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>9900-H16Q-404</Footer>
+      </Layout>
     </div>
   );
 }
