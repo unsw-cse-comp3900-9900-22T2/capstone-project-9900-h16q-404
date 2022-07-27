c5962ecc3f65f2d84933c937d7c950c77c5a2506
Ganeshram Jayaraman <j.ganeshram86@gmail.com>
2022-07-27 16:15:47 +1000

    Added delete request to hostreplies resource to allow host to delete reply.

--- api/apihandler.py
+++ api/apihandler.py
@@ -907,4 +907,51 @@ class HostReplies(Resource):
         else:
             return {"status": "Success", "message": "Updated Reply Succesfully"}
         
+    def delete(self):
+        getRequest = request.json
+        if ('token' in getRequest):
+            token = getRequest['token']
+        else:
+            return {"status": "Error", "message": "token was not Sent"}
+            
+        if ('eventId' in getRequest):
+            eventId = getRequest['eventId']
+        else:
+            return {"status": "Error", "message": "Event Id was not Sent"}
         
+        if ('targetUserId' in getRequest):
+            targetUserId = getRequest['targetUserId']
+        else:
+            return {"status": "Error", "message": "Target User Id was not Sent"}
+        
+        host_replies_delete_params = {}
+        host_replies_delete_params['replyTimeStamp'] = None
+        host_replies_delete_params['reply'] = ""
+        
+        temp_db = InitDB()
+        
+        # check user exists
+        user_exists = temp_db.check_usertoken_exists(token)
+        
+        if user_exists == False:
+            return {
+                'resultStatus': 'ERROR',
+                'message': 'User Token does not match'
+            }
+        
+        user_id = temp_db.get_host_id_from_token(token)
+        
+        user_has_reviewed = temp_db.check_user_hasComment(targetUserId, eventId)
+        
+        if user_has_reviewed == False:
+            return {
+                'resultStatus': 'ERROR',
+                'message': 'User has not reviewed before'
+            }
+        
+        new_id = temp_db.update_user_reviews(host_replies_delete_params, targetUserId, eventId)
+        
+        if new_id == -1:
+            return {"status": "Error", "message": "Could not delete reply!"}
+        else:
+            return {"status": "Success", "message": "Deleted Reply Succesfully"}
