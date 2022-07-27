59d2226044cf25ccd710f3ffc47f477696d5c7df
Dezhao Chen <dezhao.chen@student.unsw.edu.au>
2022-07-27 14:55:07 +1000

    watchlist

--- frontend/src/components/follow_button.jsx
+++ frontend/src/components/follow_button.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Spin } from 'antd';
 import axios from 'axios';
+import { LoadingOutlined } from '@ant-design/icons';
 
 /**
  * Make the Login Button a separate component
@@ -9,8 +10,53 @@ import axios from 'axios';
  * @returns the button component
  */
 
+const spinIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+export function follow_req(token, targetid) {
+  axios
+    .post('localhost:5000/follow', {
+      headers: {
+        'Content-Type': 'application/json',
+        token: token,
+      },
+      data: { target_id: targetid },
+    })
+    .then((res) => {
+      return res.data.resultStatus;
+    });
+}
+
+export function unfollow_req(token, targetid) {
+  axios
+    .delete('localhost:5000/follow', {
+      headers: {
+        'Content-Type': 'application/json',
+        token: token,
+      },
+      data: { target_id: targetid },
+    })
+    .then((res) => {
+      return res.data.resultStatus;
+    });
+}
+
+function get_follow (token, targetid) {
+  axios
+    .get('localhost:5000/follow', {
+      headers: {
+        'Content-Type': 'application/json',
+        token: token,
+      },
+      params: { target_id: targetid },
+    })
+    .then((res) => {
+      return res.data.resultStatus;
+    });
+}
+
 export default function FollowButton(userId) {
   const [follow, setFollow] = useState();
+  const [load, setLoad] = useState(false);
   const selfId = parseInt(localStorage.getItem('userId'));
   if (typeof userId === 'string') {
     userId = parseInt(userId);
@@ -31,7 +77,7 @@ export default function FollowButton(userId) {
       }
     }
     axios.get('http://127.0.0.1:5000/event?event_id=1').then((res) => {
-      console.log(res);
+      console.log(res.data.resultStatus);
     });
   }, [userId]);
   if (
@@ -55,15 +101,23 @@ export default function FollowButton(userId) {
       <>
         {selfId === userId ? (
           <Button
+            style={{ width: 100 }}
+            href={'/watchlist'}
             onClick={() => {
               console.log('to Watchlist');
             }}
           >
             Watchlist
           </Button>
+        ) : load ? (
+          <Button style={{ width: 100 }}>
+            <Spin indicator={spinIcon} />
+          </Button>
         ) : follow ? (
           <Button
+            style={{ width: 100 }}
             onClick={() => {
+              setLoad(true);
               console.log('unfollow');
               setFollow(false);
               axios
@@ -71,14 +125,20 @@ export default function FollowButton(userId) {
                 .then((res) => {
                   console.log(res);
                 });
+              setTimeout(() => {
+                console.log('Delayed for 1 second.');
+                setLoad(false);
+              }, 1000);
             }}
           >
             Unfollow
           </Button>
         ) : (
           <Button
+            style={{ width: 100 }}
             type='primary'
             onClick={() => {
+              setLoad(true);
               console.log('follow');
               setFollow(true);
               axios
@@ -86,6 +146,10 @@ export default function FollowButton(userId) {
                 .then((res) => {
                   console.log(res);
                 });
+              setTimeout(() => {
+                console.log('Delayed for 1 second.');
+                setLoad(false);
+              }, 1000);
             }}
           >
             Follow
