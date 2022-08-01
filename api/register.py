37f422cb2547d8c0c020d7704863045c8284000b
bengbmccoy <>
2022-08-01 17:24:44 +1000

    linter recommended changes for api/register.py

--- api/register.py
+++ api/register.py
@@ -1,9 +1,9 @@
-'''
+"""
 Written by: Group 404
 
 This file handles all API requests for registering a user
 
-'''
+"""
 # import third party libaries
 from flask_restful import Resource, reqparse
 
@@ -16,13 +16,13 @@ class Register(Resource):
 
         # parse request
         parser = reqparse.RequestParser()
-        parser.add_argument('email', type=str)
-        parser.add_argument('password', type=str)
-        parser.add_argument('confirm', type=str)
+        parser.add_argument("email", type=str)
+        parser.add_argument("password", type=str)
+        parser.add_argument("confirm", type=str)
         args = parser.parse_args()
 
-        request_email = args['email']
-        request_password = args['password']
+        request_email = args["email"]
+        request_password = args["password"]
 
         # create db engines
         user_db = UsersDB()
@@ -40,6 +40,9 @@ class Register(Resource):
         if new_id == -1:
             return {"status": "Error", "message": "could not register new user"}
         else:
-            final_ret = {"status": "Success", "message": "new user registerd with id = " +str(new_id)}
+            final_ret = {
+                "status": "Success", 
+                "message": "new user registerd with id = " +str(new_id)
+            }
 
         return final_ret
